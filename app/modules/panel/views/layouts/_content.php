ccec65391f95676f9c2f63eb1413f781b0aedc87
Konstantin Kotov <truck01@mail.ru>
2021-10-05 10:26:52 +0300

    Remove home link in main layout

--- app/modules/panel/views/layouts/_content.php
+++ app/modules/panel/views/layouts/_content.php
@@ -6,12 +6,6 @@
                 <div class="col-sm-6">
                     <h1 class="m-0 text-dark"><?php echo $this->title ?></h1>
                 </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="/"><?php echo t('Home')?></a></li>
-                        <li class="breadcrumb-item active"><?php echo (t(Yii::$app->params['nameEng']).' '.Yii::$app->version) ?></li>
-                    </ol>
-                </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
