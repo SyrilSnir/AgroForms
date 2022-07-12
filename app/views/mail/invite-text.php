d59cc22aeb78ad9f7f2440a520d344cdfc1ffd40
Konstantin Kotov <truck01@mail.ru>
2022-07-12 16:15:01 +0300

    Change invite user email template

--- app/views/mail/invite-text.php
+++ app/views/mail/invite-text.php
@@ -1,10 +1,61 @@
-Здравствуйте, <?= $fio ?>
-Вы можете войти на сайт, щёлкнув по
-следующей ссылке, или скопировав её в
-адресную строку браузера:
+-- Это письмо сформированно автоматически, пожалуйста, не отвечайте на него! -—
 
+Уважаемый участник выставки!
+Мы рады приветствовать Вас как участника выставки АГРОСАЛОН 2020.
+
+Это электронное письмо содержит ссылку для доступа к сервисному порталу АГРОСАЛОН-а.
+Использыя его Вы сможете заказать необходимые услуги для участия в выставке.
+
+Данные для входа не предназначены для пересылки другим лицам!
+
+Вы можете войти на порталу, щёлкнув по следующей ссылке, или скопировав её в адресную строку браузера:
 <?= $link ?>
 
-Эта одноразовая ссылка для входа на сайт
-ведёт на страницу задания нового пароля.
+Эта ссылка является одноразовой и ведёт на страницу задания нового пароля.
+Пожалуйста, пройдите по указанной ссылке или скопируйте ее в адресную строку Вашего браузера.
+Вам будет предложено ввести Ваш новый личный пароль.
+Пожалуйста, сразу запомните или запишите его!
+Восстановить его в дальнейшем будет невозможно.
+
+Ваш почтовый адрес: <?php echo $email ?>
+Прямой вход на портал: <?php echo $siteUrl ?>
+
+У вас есть вопросы? Мы будем рады помочь вам!
+
+С уважением,
+Команда АГРОСАЛОН
+Москва, МВЦ «Крокус Экспо»
+Т.: + 7 (495) 781-37-56
+Ф.: + 7 (495) 781-37-08
+www.agrosalon.ru
+
+-- This is an automatically sent e-mail, please do not reply directly to this e-mail --
+
+Dear exhibition participant!
+We are delighted to welcome you as an exhibitor at AGROSALON 2020.
+
+This e-mail contains the link to access the AGROSALON Service-Portal.
+With the AGROSALON Service-Portal you can order the necessary services for your participation in the exhibition.
+
+Login data is not intended to be forwarded to other persons!
+
+You can log in to the portal by clicking on the following link, or by copying it into your browser's address bar:
+<?= $link ?>
+
+This link is one-time and leads to the page for setting a new password.
+Please enter the link provided or copy it in the address bar of your browser.
+After having entered you will be asked to make the a personal password.
+Please save it now! In the future, it will be impossible to recover the password.
+
+Your e-mail address: <?php echo $email ?>
+At <?php echo $siteUrl ?> you can directly enter the portal.
+
+You have any questions? We'll be glad to help you!
+
+Yours sincerely,
+AGROSALON team
+Moscow, Crocus-Expo
+Тel.: + 7 (495) 781-37-56
+Fax: + 7 (495) 781-37-08
+www.agrosalon.ru
 
