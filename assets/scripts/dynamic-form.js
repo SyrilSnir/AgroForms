e680053c57e8205be4a5eb680b1a6958429c2e53
Catman <truck01@mail.ru>
2024-02-05 13:45:23 +0300

    Choose Vue to production mode

--- assets/scripts/dynamic-form.js
+++ assets/scripts/dynamic-form.js
@@ -1,5 +1,6 @@
 import DynamicFormApp from './components/vue/DynamicForm/DynamicFormApp.vue';
 const Vue = window.Vue;
+Vue.config.devtools = false;
 const appRootElement = document.getElementById('dynamic-form-app');
 const isReadOnly = !!appRootElement.dataset.readOnly;
 const contractId = parseInt(appRootElement.dataset.contractId);
