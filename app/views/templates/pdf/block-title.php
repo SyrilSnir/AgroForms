227daced8b1c45f9d5e1e70b7f01226cb5c226af
Catman <truck01@mail.ru>
2024-02-13 11:02:39 +0300

    Show full company name in pdf request title

--- app/views/templates/pdf/block-title.php
+++ app/views/templates/pdf/block-title.php
@@ -6,7 +6,7 @@ use app\models\ActiveRecord\Requests\Request;
 <table  style="width:100%; font-family: Verdana;">
     <tr style="font-size: 10pt">
         <td style="text-align: left;color:black;"><span><?php echo t('Exhibitor','exhibitions');?>: </span></td>
-        <td style="text-align: right;color:black;"><span><?php echo $model->user->company->name ?></span></td>
+        <td style="text-align: right;color:black;"><span><?php echo $model->user->company->full_name ?></span></td>
     </tr>
     <tr style="font-size: 18pt">
         <td style="font-size: 18pt;text-align: left;color:black;"><span><?php echo $model->form->name ?></span></td>
